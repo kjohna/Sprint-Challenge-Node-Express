7d6074b22a251b643320272c1abf44298e273180
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-15 09:56:26 -0700

    GET project by id working

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -17,4 +17,19 @@ router.get('/', async(req, res) => {
   }
 });
 
+// GET post by ID
+router.get('/:id', async(req, res) => {
+  try {
+    const project = await projectsDb.get(req.params.id);
+    console.log("project: ", project);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: "No project with that id was found." });
+    }
+  } catch {
+    res.status(500).json({ error: "The project could not be retrieved." });
+  }
+});
+
 module.exports = router;
